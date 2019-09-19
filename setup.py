55876bb01d96f49d7d011b322c9efd7ead9de4e4
Joppe Blondel <joppe@blondel.nl>
2019-09-19 15:44:55 +0200

    Updated files for new name: qandaomr
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- setup.py
+++ setup.py
@@ -4,14 +4,29 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='omr',
-    version='1.1',
-    description='Python and OpenCV OMR (optical mark recognition)',
+    name='qandaomr',
+    version='1.2',
+    description='Q&A Python and OpenCV OMR (optical mark recognition)',
     long_description=long_description,
+
     author='Joppe Blondel',
     author_email='joppe@blondel.nl',
+    download_url='https://github.com/Jojojoppe/qandaomr/archive/v1.2.tar.gz',
+    url='https://github.com/Jojojoppe/qandomr',
+
+    keywords = ['OMR', 'Optical Mark Recognition',],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+  ],
+
     packages=['omr'],
-    licence='GPLv3',
+    licence='GNU General Public License v3 (GPLv3)',
     install_requires=['opencv-python', 'numpy', 'argparse', 'zxing'],
-    scripts=['scripts/omr']
+    scripts=['scripts/qandaomr']
 )
